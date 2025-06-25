78bb6ed0284d4650954c5b463fa94c1fca8c82c2
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-25 09:57:06 +0530

    setting the size for the extension popup

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <div className='w-[380px] h-[600px]'>
     <App />
+    </div>
   </StrictMode>,
 )
