1a850cf40d76c5a998ae0972cab1437252692419
Satyam Shivhare <mkssshivhare@gmail.com>
2025-06-26 08:14:40 +0530

    some changes to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className='w-[380px] h-[600px]'>
+    <div className='w-[380px] h-[600px] p-0 m-0'>
     <App />
     </div>
   </StrictMode>,
